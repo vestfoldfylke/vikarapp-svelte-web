75666559254bb4966c89c02180b8a0cc9e1f26cc
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2026-01-08 20:29:23 +0100

    feat: Show how many times a substitution has been extended

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,13 +34,19 @@
             return
         }
 
+        let hasExtendedSubstitutions = false
         for (const substitution of response.data) {
+          if (substitution.substitutionUpdated > 0) {
+            hasExtendedSubstitutions = true
+          }
+
           data.push({
             statusReadable: convertStatus(substitution.status),
             substituteName: substitution.substituteName,
             teacherName: substitution.teacherName,
             teamName: substitution.teamName,
             expirationTimestamp: convertDate(substitution.expirationTimestamp),
+            extendedCount: substitution.substitutionUpdated > 0 ? substitution.substitutionUpdated : '',
             _id: substitution._id,
             status: substitution.status,
             substituteUpn: substitution.substituteUpn,
@@ -48,7 +54,10 @@
             teamId: substitution.teamId
           })
         }
-        columnHeaders = ['Status', 'Vikar', 'Lærer', 'Klasser', 'Utløper'] // TODO: Antall ganger forlenget
+        columnHeaders = ['Status', 'Vikar', 'Lærer', 'Klasser', 'Utløper']
+        if (hasExtendedSubstitutions) {
+            columnHeaders.push('Antall forlengelser')
+        }
     }
 
     onMount(async () => {
